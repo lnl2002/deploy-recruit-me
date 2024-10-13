925e6b3efa9879b7e2b8f159e2b25494930dc679
Lai Ngoc Lam <laingoclam3112@gmail.com>
2024-10-13 20:59:52 +0700

    swagger config + update status apply

--- BACK_END/src/routes/index.ts
+++ BACK_END/src/routes/index.ts
@@ -1,14 +1,19 @@
 import { Express } from 'express'
+import swaggerUi from 'swagger-ui-express';
 import Test from './tests'
 import Auth from './auth'
 import jobRouter from './jobRouter'
 import MeetingRouter from './meetingRoom'
+import ApplyRouter from './apply'
+import swaggerSpec from '../swagger';
 
 const routes = (app: Express) => {
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
     app.use('/api/v1/test', Test)
     app.use('/api/v1/auth', Auth)
     app.use('/api/v1/jobs', jobRouter)
     app.use('/api/v1/meeting-room', MeetingRouter)
+    app.use('/api/v1/applies', ApplyRouter)
 }
 
 export default routes
