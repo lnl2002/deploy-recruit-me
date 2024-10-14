e3be3a43bffc3f76305bc07bb73fa4a1b69f50ed
Lai Ngoc Lam <laingoclam3112@gmail.com>
2024-10-14 20:59:11 +0700

    change location model

--- BACK_END/src/models/locationModel.ts
+++ BACK_END/src/models/locationModel.ts
@@ -1,13 +1,31 @@
 import mongoose, { Document, Schema } from 'mongoose'
 
 export interface ILocation extends Document {
+    country: string
     city: string
+    district: string
+    ward: string
+    detailLocation: string
 }
 
 const locationSchema: Schema = new Schema({
+    country: {
+        type: String,
+        required: true,
+    },
     city: {
         type: String,
         required: true,
+    },
+    district: {
+        type: String,
+        required: true,
+    },
+    ward: {
+        type: String,
+    },
+    detailLocation: {
+        type: String,
     },
 })
 
