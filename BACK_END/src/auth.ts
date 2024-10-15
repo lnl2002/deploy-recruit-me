07267d2d52822ddbff2d461b400a9fec37ef2521
Lai Ngoc Lam <laingoclam3112@gmail.com>
2024-10-15 10:51:16 +0700

    security

--- BACK_END/src/auth.ts
+++ BACK_END/src/auth.ts
@@ -16,10 +16,11 @@ passport.use(
                 const email = profile?.emails[0]?.value || '';
 
                 if (!email || !validator.isEmail(email)) {
-                    throw new Error('Email không hợp lệ')
+                    throw new Error('Invalid email')
                 }
+                const sanitizedEmail = String(email).trim()
 
-                let user: IAccount = await Account.findOne({ email }).populate('role');
+                let user: IAccount = await Account.findOne({ sanitizedEmail }).populate('role');
 
 
                 if (!user) {
@@ -29,7 +30,7 @@ passport.use(
                     user = await Account.create({
                         googleId: profile.id,
                         name: profile.displayName,
-                        email: profile.emails[0].value,
+                        email: sanitizedEmail,
                         role: defaultRole._id,
                         image: profile?.photos[0]?.value || ''
                     })
