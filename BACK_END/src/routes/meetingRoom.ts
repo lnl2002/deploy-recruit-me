49c15b6d34ec6c988f9e674bb3bb8ff4f6f05ec4
Lai Ngoc Lam <laingoclam3112@gmail.com>
2024-10-15 10:13:14 +0700

    Done get schedules

--- BACK_END/src/routes/meetingRoom.ts
+++ BACK_END/src/routes/meetingRoom.ts
@@ -3,6 +3,12 @@ import meetingController from '../controllers/meetingRoom'
 
 const router = express.Router()
 
+// GET
+router.get('/schedules', meetingController.getInterviewSchedules)
+
+// POST
+
+// PUT
 router.put('/update-status', meetingController.updateMeetingStatus)
 
 export default router
