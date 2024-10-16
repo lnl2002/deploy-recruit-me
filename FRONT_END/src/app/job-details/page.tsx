382e829857ec73c2933add5e903974adb560a3d1
Lai Ngoc Lam <laingoclam3112@gmail.com>
2024-10-17 00:06:18 +0700

    fix deploy error

--- FRONT_END/src/app/job-details/page.tsx
+++ FRONT_END/src/app/job-details/page.tsx
@@ -1,14 +1,17 @@
 "use client";
 
-import React from "react";
+import React, { Suspense } from "react";
 import { MainLayout } from "@/components";
 import JobDetails from "@/screens/jobDetails";
+import { Spinner } from "@nextui-org/react";
 
 export default function JobDetailsPage() {
   return (
     <main>
       <MainLayout>
-        <JobDetails />
+        <Suspense fallback={<Spinner label="Loading..." color="primary" />}>
+          <JobDetails />
+        </Suspense>
       </MainLayout>
     </main>
   );
