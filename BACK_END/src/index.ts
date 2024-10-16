b09d663108c6d377ced711c031a07355d6c4b8fa
Lai Ngoc Lam <laingoclam3112@gmail.com>
2024-10-17 00:38:57 +0700

    log error

--- BACK_END/src/index.ts
+++ BACK_END/src/index.ts
@@ -35,7 +35,7 @@ app.get('/check-connection', async (req, res) => {
         await mongoose.connection.db.admin().ping()
         res.status(200).send('Connected to MongoDB Atlas')
     } catch (error) {
-        res.status(500).send(error)
+        res.status(500).send(error.toString())
     }
 })
 
