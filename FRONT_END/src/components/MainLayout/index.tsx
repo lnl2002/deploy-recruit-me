eb80738d81c4f246cf466757082b644da8070acc
PhuongThan25503 <142188056+PhuongThan25503@users.noreply.github.com>
2024-10-30 20:08:52 +0700

    update: hr header

--- FRONT_END/src/components/MainLayout/index.tsx
+++ FRONT_END/src/components/MainLayout/index.tsx
@@ -1,17 +1,2 @@
-import React from "react";
-import { Header } from "../Header";
-import { Footer } from "../Footer";
-
-type MainLayoutProp = {
-  children: React.ReactNode;
-};
-
-export const MainLayout = ({ children }: MainLayoutProp): React.JSX.Element => {
-  return (
-    <div className="w-full fixed-container bg-themeWhite">
-      <Header></Header>
-      {children}
-      <Footer></Footer>
-    </div>
-  );
-};
+export * from "./hrLayout";
+export * from "./mainLayout";
