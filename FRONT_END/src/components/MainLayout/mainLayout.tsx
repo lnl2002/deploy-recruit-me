c593f4c960cd7f8cd82c3d1377e3f5dc4a7f7564
Phuong Quang Than <64473652+garenaobama@users.noreply.github.com>
2024-11-25 13:01:19 +0700

    update: ai chat basic

--- FRONT_END/src/components/MainLayout/mainLayout.tsx
+++ FRONT_END/src/components/MainLayout/mainLayout.tsx
@@ -1,6 +1,8 @@
+"use client"
 import React from "react";
 import { Header } from "../Header";
 import { Footer } from "../Footer";
+import { AIChat } from "../AiChat";
 
 type MainLayoutProp = {
   children: React.ReactNode;
@@ -10,7 +12,12 @@ export const MainLayout = ({ children }: MainLayoutProp): React.JSX.Element => {
   return (
     <div className="w-full fixed-container bg-themeWhite h-full">
       <Header></Header>
-      <div className="min-h-screen">{children}</div>
+      <div className="min-h-screen">
+        {children}
+        <div className="fixed right-10 bottom-10">
+          <AIChat />
+        </div>
+      </div>
       <Footer></Footer>
     </div>
   );
--- FRONT_END/src/lotties/index.ts
+++ FRONT_END/src/lotties/index.ts
@@ -1,9 +1,11 @@
 import Loading from './loading.json'
 import Success from './done.json'
 import Error from './error.json'
+import AIBot from './ai_bot.json'
 
 export const LottieApp = {
     Loading : Loading,
     Success: Success,
-    Error: Error
+    Error: Error,
+    AIBot: AIBot
 }
