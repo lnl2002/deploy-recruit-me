cf27d55b6c61e5fc1dabcc63eba39eb00c68db79
Phuong Quang Than <64473652+garenaobama@users.noreply.github.com>
2024-12-02 15:16:31 +0700

    notification

--- BACK_END/src/controllers/systemController.ts
+++ BACK_END/src/controllers/systemController.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express'
-import { mailService } from "../services/mailServices/mailService"
+import { mailService } from '../services/mailServices/mailService'
 import { genAnswer } from '../configs/gemini-chat-config'
+import Notification from '../models/notificationModel'
 
 export const systemController = {
     mutipleMail: async (req: Request, res: Response, next: NextFunction): Promise<Response> => {
@@ -9,12 +10,12 @@ export const systemController = {
             const info = mailService.sendMailBase({
                 subject,
                 body: body,
-                sendTo: receivers
+                sendTo: receivers,
             })
-            return res.status(200).json({ message: 'Email sent successfully' });
+            return res.status(200).json({ message: 'Email sent successfully' })
         } catch (error: unknown) {
             next(error)
-            return res.status(400).json({ message: 'Email not sent' });
+            return res.status(400).json({ message: 'Email not sent' })
         }
     },
 
@@ -22,13 +23,47 @@ export const systemController = {
         try {
             const { input, history } = req.body
             const respond = await genAnswer(input, history)
-            console.log(respond);
-            
+            console.log(respond)
+
             const cleanedResult = respond.response.text().replace('```json', '').replace('```', '')
-            return res.status(200).json(JSON.parse(cleanedResult));
+            return res.status(200).json(JSON.parse(cleanedResult))
         } catch (error: unknown) {
             // next(error)
-            return res.status(400).json({ message: 'AI not sent' });
+            return res.status(400).json({ message: 'AI not sent' })
+        }
+    },
+
+    createNotification: async (req: Request, res: Response, next: NextFunction): Promise<Response> => {
+        try {
+            const { receiver, content, url } = req.body
+            const notification = new Notification({ receiver, content, url, seen: false })
+            await notification.save()
+            return res.status(201).json(notification)
+        } catch (error) {
+            res.status(500).json({ error: 'Error when create noti: ' + error })
+        }
+    },
+
+    getUserNotifications: async (req: Request, res: Response, next: NextFunction): Promise<Response> => {
+        try {
+            const userId = req.user._id
+            const notifications = await Notification.find({ receiver: userId }).sort({seen:1, createdAt: -1 }).limit(10)
+            return res.status(200).json(notifications)
+        } catch (error) {
+            res.status(500).json({ error: 'Error when get noti: ' + error })
+        }
+    },
+
+    markAsSeen: async (req: Request, res: Response, next: NextFunction): Promise<Response> => {
+        try {
+            const { notificationId } = req.params
+            console.log(notificationId);
+            
+            const updates = req.body
+            const notification = await Notification.findByIdAndUpdate( notificationId, { $set: { seen: true } }, { new: true } );
+            return res.status(200).json(notification)
+        } catch (error) {
+            res.status(500).json({ error: error })
         }
     },
 }
--- BACK_END/src/routes/systemRouter.ts
+++ BACK_END/src/routes/systemRouter.ts
@@ -1,5 +1,6 @@
 import express, { Router } from 'express'
 import { systemController } from '../controllers/systemController'
+import { requireRole } from '../middlewares/auth'
 
 const systemRouter: Router = express.Router()
 
@@ -13,6 +14,12 @@ example body
 */
 systemRouter.post('/ai-chat', systemController.getAIAnswer)
 
+/*email*/
 systemRouter.post('/mail/send', systemController.mutipleMail)
+
+/*notification*/
+systemRouter.post('/notifications', systemController.createNotification) // Route to create a new notification
+systemRouter.get('/notifications', requireRole(['CANDIDATE']), systemController.getUserNotifications) // Route to get notifications for a user
+systemRouter.patch('/notifications/:notificationId/seen', systemController.markAsSeen) // Route to mark a notification as seen
 
 export default systemRouter
